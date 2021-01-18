cc2962aff3eaf18ddcde0d65776809111a039d8c
Snehil <snehilverma488@gmail.com>
2021-01-18 19:33:29 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        //GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid) //server side validation of name and displayorder
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            return View(obj);
+        }
+
+        //POST - Edit
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        //annotation tells it is primary key and identity column (auto increments)
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please fill out this field.")]
+        public string Name { get; set; }
+        [Range(1,int.MaxValue,ErrorMessage ="Enter a valid number")]
+        public int DisplayOrder { get; set; }
+    }
+}
